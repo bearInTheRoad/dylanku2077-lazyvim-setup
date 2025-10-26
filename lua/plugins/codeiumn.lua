f3b532f309534993908503f208d0f6811cede9a9
dylan <luodaojun1128@gmail.com>
2025-10-26 16:53:16 +0800

    Added some config for codeium plugin

--- lua/plugins/codeiumn.lua
+++ lua/plugins/codeiumn.lua
@@ -1,32 +1,27 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    opts = {
-      enable_chat = true,
-    },
-    config = function(_, opts)
-      require("codeium").setup(opts)
-      
-      local last_tab_time = 0
-      local tab_threshold = 300
-      
-      vim.keymap.set('i', '<Tab>', function()
-        local current_time = vim.uv.now()
-        local time_diff = current_time - last_tab_time
-        local codeium = require("codeium.virtual_text")
-        
-        if time_diff < tab_threshold and codeium.get_current_completion_item() ~= nil then
-          last_tab_time = 0
-          require("codeium.virtual_text").accept()
-        else
-          last_tab_time = current_time
-          if vim.fn.pumvisible() == 1 then
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, false, true), 'n', false)
-          else
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
-          end
-        end
-      end, { silent = true })
-    end,
-  },
+  "Exafunction/codeium.nvim",
+  event = "InsertEnter",
+  config = function()
+    require("codeium").setup({
+      -- ✅ Only ghost-text
+      virtual_text = {
+        enabled = true,
+        idle_delay = 10,
+        map_keys = true, -- custom keymaps below
+        key_bindings = {
+          accept = "<leader>n",
+          accept_word = "<leader>w",
+          accept_line = "<leader>l",
+          next = "<leader>j",
+          prev = "<leader>k",
+        },
+      },
+
+      -- ✅ Manual trigger only
+      manual = true,
+
+      -- ✅ Disable cmp popup mode
+      enable_cmp_source = false,
+    })
+  end,
 }
