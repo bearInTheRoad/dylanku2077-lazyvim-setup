0b49671152b4e0e4617730e349cfdbe5c8ce8a74
dylan <luodaojun1128@gmail.com>
2026-06-21 17:00:04 +0800

    adjust the hightlight colors

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,6 +1,15 @@
 return {
 	-- 1. install the colorscheme plugin
-	{ "scottmckendry/cyberdream.nvim" },
+	{
+		"scottmckendry/cyberdream.nvim",
+		opts = {
+			overrides = function(colors)
+				return {
+					Visual = { bg = "#81a1c1", fg = "NONE", bold = true },
+				}
+			end,
+		},
+	},
 
 	-- 2. tell LazyVim to apply it
 	{
